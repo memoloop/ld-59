86d393fa6cefd16c6e84a6878fd6c52b64707ab6
memoloop <mariomemoli36@gmail.com>
2026-05-29 16:59:25 +0200

    feat: expanded level 2

--- scenes/levels/level_2.gd
+++ scenes/levels/level_2.gd
@@ -2,7 +2,11 @@ extends Level
 
 @onready var portal_2: Portal = $FourthSection/Portal2
 @onready var portal_3: Portal = $FourthSection/Portal3
+@onready var portal_4: Portal = $FourthSection/Portal4
 
 func _on_signal_receiver_activate_changed(value: bool) -> void:
 	portal_2.activated = value
 	portal_3.activated = value
+
+func _on_lever_activate_changed(value: bool) -> void:
+	portal_4.activated = value
